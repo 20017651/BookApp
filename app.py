07eebebe7184b8e218cf62c333c61df7703fb86f
20017651 <152613045+20017651@users.noreply.github.com>
2023-12-14 02:33:20 +0000

    jiss

--- app.py
+++ app.py
@@ -350,4 +350,4 @@ def update():
   #  app.run(debug=True)
 
 if __name__ == "__main__":
- app.run(host='0.0.0.0', port='8080')
+ app.run(host='0.0.0.0',port='8080', ssl_context=('cert.pem', 'privkey.pem')) #Run the flask app at port 8080
